6ce7dc4fed933d709ee62a42d64407fe4d094ff9
kennethk89 <kennethk89@gmail.com>
2018-06-01 17:27:29 -0700

    started login functionality, added bcrypt and jwt token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://kennethk89:Oldmanteeth1@ds245250.mlab.com:45250/devconnector"
+    "mongodb://kennethk89:Oldmanteeth1@ds245250.mlab.com:45250/devconnector",
+  secretOrKey: "secret"
 };
